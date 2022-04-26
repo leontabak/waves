ed9a07d466705e9ababe8752f9bbbe038e66176c
Leon Hannah Tabak <l.tabak@ieee.org>
2022-04-26 09:45:35 -0500

    Extend JFrame, configure JFrame.

--- src/edu/cornellcollege/Waves.java
+++ src/edu/cornellcollege/Waves.java
@@ -1,8 +1,21 @@
 package edu.cornellcollege;
 
-public class Waves {
+import javax.swing.JFrame;
+
+public class Waves extends JFrame {
+    private static final int FRAME_WIDTH = 768;
+    private static final int FRAME_HEIGHT = 768;
+    private static final String FRAME_TITLE = "Waves";
+
+    public Waves() {
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle(FRAME_TITLE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    } // Waves()
 
     public static void main(String[] args) {
-	    System.out.println( "Hello!" );
+        Waves waves = new Waves();
     } // main( String [] )
 } // Waves
